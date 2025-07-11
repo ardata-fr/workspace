8a884d0474e577b6aef14d3473adf94cf6cbe1ac
David Gohel <david.gohel@ardata.fr>
2025-07-11 14:47:43 +0200

    feat: add new function `delete_dataset()`

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# workspace 0.1.2
+
+## Features
+
+- add new function `delete_dataset()` to delete datasets from workspace.
--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,7 @@
-#' @importFrom utils packageVersion
+#' @importFrom utils packageVersion globalVariables
 pkg_version_str <- function(package_name) {
   pkg_version <- packageVersion(package_name)
   format(pkg_version)
 }
+
+globalVariables(c(".data"))
